9746ccbdb312613c0f8409c6dfba1b5cfd90ccb5
achyuthblaji <achyuthbalaji97@gmail.com>
2022-04-02 19:59:53 -0400

    cacheing and ranking

--- PageX/src/SearchEngineRunner.java
+++ PageX/src/SearchEngineRunner.java
@@ -1,15 +1,57 @@
 
+import java.io.File;
+
 import java.io.IOException;
+import java.util.Map.Entry;
+
+import org.apache.commons.io.FileUtils;
 import java.util.*;
+import tf_idf.Count_heap;
+import Caching.LRUCache;
+import org.apache.commons.io.FileUtils;
 public class SearchEngineRunner {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		Crawler.webCrawl(1,30,"https://www.geeksforgeeks.org/",new ArrayList<String>());
+		List<String>ranking_list=new ArrayList<String>();
+		
+		Arrays.stream(new File("./htmlpages/").listFiles()).forEach(File::delete);
+		Arrays.stream(new File("./htmlToTextPages/").listFiles()).forEach(File::delete);
+		Arrays.stream(new File("./cached_files/").listFiles()).forEach(File::delete);
+		System.out.println("Done");
+		
+		HashMap<String, String> urlDict=Crawler.webCrawl(1,5,"https://en.wikipedia.org/wiki/Computer",new ArrayList<String>());
+		System.out.println(Arrays.asList(urlDict)); 
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the word to be searched");
 		String word=sc.nextLine();
-		SearchingWord.searchWord(word);
+		ranking_list=SearchingWord.searchWord(word,"./htmlToTextPages/");
+		
+
+		System.out.println("rankingList");
+		for(String fruit:ranking_list)  
+			  System.out.println(fruit);  
+		LRUCache cache = new LRUCache();
+		System.out.println("Enter number of pages for caching purpose which should be less than   "+ranking_list.size());
+		String total_cach_pages=sc.nextLine();
+		
+		for(int i=0;i<Integer.parseInt(total_cach_pages);i++) {
+			String index=sc.nextLine();
+			cache.putElementInCache(Integer.parseInt(index), ranking_list.get(Integer.parseInt(index)));
+			
+			
+		}
+		
+		cache.populate_cache_pages();
+		cache.add_cached_pages(urlDict);
+		System.out.println("Enter word to be searched in cache memory");
+		String cache_word=sc.nextLine();
+		System.out.println("rankingList");
+		ranking_list=SearchingWord.searchWord(cache_word,"./cached_files/");
+		 
+	}
+	
+	
+		
 	}
 
-}
+
--- PageX/src/SearchingWord.java
+++ PageX/src/SearchingWord.java
@@ -1,36 +1,70 @@
 import textprocessing.*;
+import tf_idf.tf_idf_new;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 public class SearchingWord {
 	
 	public static HashMap<String, Integer> urlOccurrences = new HashMap<String, Integer>(); // This will tell no. of occurences of a word
 	public static HashMap<String, Integer> urlWordCount = new HashMap<String, Integer>(); // This will tell no. of words in a link.
-
-	public static void searchWord(String word) throws IOException {
-		File folder = new File("./htmlToTextPages/");
-		
+	public static HashMap<String,Double> tf_idf_score = new HashMap<String,Double>();
+	public static List<String>ranking_list=new ArrayList<String>();
+	
+	public static List<String> searchWord(String word,String folder_address) throws IOException {
+		System.out.println(folder_address);
+		System.out.println(word);
+		File folder = new File(folder_address);
+		Integer documents_containing_key_word =0;
 		File[] allFiles = folder.listFiles();
-		//System.out.println(allFiles.length);
+		System.out.println("length of files");
+		System.out.println(allFiles.length);
 		for(int i=0;i<allFiles.length;i++) {
+			System.out.println("file name");
+			System.out.println(allFiles[i]);
 	        
 	        int count=wordSearch(allFiles[i],word);
 	        System.out.println(Crawler.urlDict.get(allFiles[i].getName())+" number of occurences: "+ count);
 			urlOccurrences.put(Crawler.urlDict.get(allFiles[i].getName()),count);
+			if (count>=1) {
+				documents_containing_key_word=documents_containing_key_word+1;				
+			}
+			
 		}
+		System.out.println("documents_containing_key_word");
+		System.out.println(documents_containing_key_word);
+		System.out.println(urlOccurrences.size());
+		System.out.println(urlWordCount.size());
 		
+		tf_idf_score=tf_idf_new.compute_tf_idf(urlOccurrences,urlWordCount,documents_containing_key_word);
+		ranking_list=tf_idf_new.populate_heap(tf_idf_score);
+		urlOccurrences.clear();
+		urlWordCount.clear();
+		tf_idf_score.clear();
+		
+		return ranking_list;
 		
 	       
 		
 	}
+	
+	
+	
+	
 	public static int searchBruteForce(String word, String pattern) {
 
 		int offset=BruteForceMatch.search1(word, pattern);
 		return offset;
 	}
+	
 	public static int wordSearch(File filePath, String word)
 	{
 		int count=0;
@@ -71,5 +105,7 @@ public class SearchingWord {
 		}
 		return count;
 	}
+	
+	
 
 }
