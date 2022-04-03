0d4659e213751704cfb081d1c57b9edd64ce83f5
Piyush Mehta <me@piyushmehta.com>
2022-04-03 17:02:08 -0400

    Update LRUCache.java

--- PageX/src/Caching/LRUCache.java
+++ PageX/src/Caching/LRUCache.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;  
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -14,111 +14,102 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.apache.commons.io.FileUtils;  
- class Cache   
-{  
-  int key;               
-  String value;           
- Cache(int key, String value) {  
-    this.key = key;  
-    this.value = value;  
-  }  
-}  
- public class LRUCache {  
- static Deque<Integer> q = new LinkedList<>();   
- static LRUCache cache = new LRUCache(); 
-  static Map<Integer, Cache> map = new HashMap<>();  
-  int CACHE_CAPACITY = 4;  
-  static List<String> cached_file_names=new ArrayList<String>();
-  public String getElementFromCache(int key)   
-  {  
-   if(map.containsKey(key))   
-    {  
-      Cache current = map.get(key);  
-      q.remove(current.key);  
-      q.addFirst(current.key);  
-      return current.value;  
-    }   
-  return "Not exist";  
-  }  
- public void putElementInCache(int key, String value)   
-  {  
-    if(map.containsKey(key))   
-    {  
-      Cache curr = map.get(key);      
-      q.remove(curr.key);                   
-    }  
-    else   
-    {  
-      if(q.size() == CACHE_CAPACITY)   
-      {  
-        int temp = q.removeLast();    
-        map.remove(temp);  
-      }  
-    }  
-    Cache newItem = new Cache(key, value);  
-    q.addFirst(newItem.key);     
-    map.put(key, newItem);  
-  }  
- public void populate_cache_pages() {
-	 for (Integer item: q) {
-		 Cache current = map.get(item);
-//         System.out.println(current.value);
-         cached_file_names.add(current.value);
-     }
-	 System.out.println("cached_file_names");
-	 for(String fruit:cached_file_names)  
-		  System.out.println(fruit);
-	
- }
- public void copy_files(String Source,String Destination) {
-	 
-	 File source = new File(Source);
-	 File dest = new File(Destination);
-	 try {
-	     FileUtils.copyFile(source, dest);
-	 } catch (IOException e) {
-	     e.printStackTrace();
-	 }
- }
- public void add_cached_pages(HashMap<String, String> urlDict) {
-	 String file_source="./htmlToTextPages/";
-	 String File_destination="./cached_files/";
-	 for(Entry<String, String> entry_1: urlDict.entrySet()) {
+import org.apache.commons.io.FileUtils;
+class Cache {
+    int key;
+    String value;
+    Cache(int key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+public class LRUCache {
+    static Deque < Integer > q = new LinkedList < > ();
+    static LRUCache cache = new LRUCache();
+    static Map < Integer, Cache > map = new HashMap < > ();
+    int CACHE_CAPACITY = 4;
+    static List < String > cached_file_names = new ArrayList < String > ();
+    public String getElementFromCache(int key) {
+        if (map.containsKey(key)) {
+            Cache current = map.get(key);
+            q.remove(current.key);
+            q.addFirst(current.key);
+            return current.value;
+        }
+        return "Not exist";
+    }
+    public void putElementInCache(int key, String value) {
+        if (map.containsKey(key)) {
+            Cache curr = map.get(key);
+            q.remove(curr.key);
+        } else {
+            if (q.size() == CACHE_CAPACITY) {
+                int temp = q.removeLast();
+                map.remove(temp);
+            }
+        }
+        Cache newItem = new Cache(key, value);
+        q.addFirst(newItem.key);
+        map.put(key, newItem);
+    }
+    public void populate_cache_pages() {
+        for (Integer item: q) {
+            Cache current = map.get(item);
+            //         System.out.println(current.value);
+            cached_file_names.add(current.value);
+        }
+        System.out.println("cached_file_names");
+        for (String fruit: cached_file_names)
+            System.out.println(fruit);
 
-	      // if give value is equal to value from entry
-	      // print the corresponding key
-		 for (String element : cached_file_names)
-		 {
-			 
-	      if(entry_1.getValue() == element) {
-	        System.out.println("The key for value "+entry_1.getKey());
-	        String s_file_name=file_source+entry_1.getKey();
-	        String d_file_name=File_destination+entry_1.getKey();
-	        copy_files(s_file_name,d_file_name);
-	        break;
-	        
-	      }
-	    }
-	 }
-	 
- }
-//public static void main(String[] args)   
-//  {  
-//     
-//    cache.putElementInCache(1, "Value_1");  
-//    cache.putElementInCache(2, "Value_2");  
-//    cache.putElementInCache(3, "Value_3");  
-//    cache.putElementInCache(4, "Value_4");  
-//    System.out.println(cache.getElementFromCache(2));  
-//    System.out.println();    
-//    System.out.println(q);  
-//    System.out.println();  
-//    System.out.println(cache.getElementFromCache(5));  
-//    cache.putElementInCache(5,"Value_5");  
-//    System.out.println();  
-//    System.out.println(q);  
-//    System.out.println();  
-//    cache.display();
-//   }  
- }  
+    }
+    public void copy_files(String Source, String Destination) {
+
+        File source = new File(Source);
+        File dest = new File(Destination);
+        try {
+            FileUtils.copyFile(source, dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void add_cached_pages(HashMap < String, String > urlDict) {
+        String file_source = "./htmlToTextPages/";
+        String File_destination = "./cached_files/";
+        for (Entry < String, String > entry_1: urlDict.entrySet()) {
+
+            // if give value is equal to value from entry
+            // print the corresponding key
+            for (String element: cached_file_names) {
+
+                if (entry_1.getValue() == element) {
+                    System.out.println("The key for value " + entry_1.getKey());
+                    String s_file_name = file_source + entry_1.getKey();
+                    String d_file_name = File_destination + entry_1.getKey();
+                    copy_files(s_file_name, d_file_name);
+                    break;
+
+                }
+            }
+        }
+
+    }
+    //public static void main(String[] args)   
+    //  {  
+    //     
+    //    cache.putElementInCache(1, "Value_1");  
+    //    cache.putElementInCache(2, "Value_2");  
+    //    cache.putElementInCache(3, "Value_3");  
+    //    cache.putElementInCache(4, "Value_4");  
+    //    System.out.println(cache.getElementFromCache(2));  
+    //    System.out.println();    
+    //    System.out.println(q);  
+    //    System.out.println();  
+    //    System.out.println(cache.getElementFromCache(5));  
+    //    cache.putElementInCache(5,"Value_5");  
+    //    System.out.println();  
+    //    System.out.println(q);  
+    //    System.out.println();  
+    //    cache.display();
+    //   }  
+}
