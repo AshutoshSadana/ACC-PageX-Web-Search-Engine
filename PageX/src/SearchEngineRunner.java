5a3e26c64fbe35d9f2cfbb6582e55af7f7cd7e93
piyush97 <me@piyushmehta.com>
2022-04-07 18:31:55 -0400

    fixes

--- PageX/src/SearchEngineRunner.java
+++ PageX/src/SearchEngineRunner.java
@@ -11,7 +11,7 @@ public class SearchEngineRunner {
 
     public static List < String > search_function(String search_word, List < String > ranking_list, String folder_name) throws IOException {
         ranking_list = SearchingWord.searchWord(search_word, folder_name);
-        System.out.println("rankingList");
+        System.out.println("List of files in order of occurances (Desc)");
         if (ranking_list.size() > 1) {
             for (String page: ranking_list) {
                 if (page != "sample")
@@ -37,90 +37,94 @@ public class SearchEngineRunner {
         System.out.println("Previous files cleared!");
 
         HashMap < String, String > urlDict = Crawler.webCrawl(1, 10, "https://piyushmehta.com", new ArrayList < String > ());
-        //System.out.println(Arrays.asList(urlDict)); 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("====================Enter your option====================");
-        System.out.println("1 for searching word");
-        System.out.println("2 for searching word in cache");
-        System.out.println("3 for providing sugesstions to word");
+        try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("====================Enter your option====================");
+			System.out.println("1 for searching word");
+			System.out.println("2 for searching word in cache");
+			System.out.println("3 for providing sugesstions to word");
 
-        Integer choice = sc.nextInt();
-        int exitStatus = 0;
-        do {
-            switch (choice) {
-                case 1:
-                    System.out.println("Enter the word to be searched");
-                    Scanner sc_1 = new Scanner(System.in);
-                    String search_word = sc_1.nextLine();
-                    listOfPagesWithRank = search_function(search_word, listOfPagesWithRank, "./htmlToTextPages/");
-                    break;
-                    /**
-                     * Case 2 caching by Piyush
-                     * @author piyushmehta
-                     */
-                case 2:
-                    for (String page: listOfPagesWithRank) {
-                        //					Printing everything except sample just for testing
-                        if (page != "sample")
-                            System.out.println(page);
-                    }
+			Integer choice = sc.nextInt();
+			int exitStatus = 0;
+			do {
+			    switch (choice) {
+			        case 1:
+			            System.out.println("Enter the word to be searched");
+			            Scanner sc_1 = new Scanner(System.in);
+			            String search_word = sc_1.nextLine();
+			            listOfPagesWithRank = search_function(search_word, listOfPagesWithRank, "./htmlToTextPages/");
+			            break;
+			            /**
+			             * Case 2 caching by Piyush
+			             * @author piyushmehta
+			             */
+			        case 2:
+			            for (String page: listOfPagesWithRank) {
+			                //					Printing everything except sample just for testing
+			                if (page != "sample")
+			                    System.out.println(page);
+			            }
 
-                    LRUCache lruCache = new LRUCache();
-                    System.out.println("Enter number of pages for caching purpose which should be less than   " + ((listOfPagesWithRank.size()) - 1));
+			            LRUCache lruCache = new LRUCache();
+			            System.out.println(listOfPagesWithRank.size() <= 0 ?
+			            		"Please first search :)" :"Enter number of pages for caching purpose which should be less than "
+			            		+ ((listOfPagesWithRank.size()) - 1));
 
-                    Scanner integerInput = new Scanner(System.in);
+			            Scanner integerInput = new Scanner(System.in);
 
-                    Integer totalPagesToBeCached = integerInput.nextInt();
+			            Integer totalPagesToBeCached = integerInput.nextInt();
 
-                    for (int i = 0; i < totalPagesToBeCached; i++) {
-                        System.out.println("Enter index of ranked list page");
-                        Integer index = integerInput.nextInt();
-                        lruCache.putElementInCache(index, listOfPagesWithRank.get(index));
-                    }
+			            for (int i = 0; i < totalPagesToBeCached; i++) {
+			                System.out.println("Enter index of ranked list page");
+			                Integer index = integerInput.nextInt();
+			                lruCache.putElementInCache(index, listOfPagesWithRank.get(index));
+			            }
 
-                    lruCache.getFileNameFromCache();
+			            
+			            lruCache.getFileNameFromCache();
 
-                    lruCache.storeToFolder(urlDict);
+			            lruCache.storeToFolder(urlDict);
 
-                    Scanner wordScanner = new Scanner(System.in);
+			            Scanner wordScanner = new Scanner(System.in);
 
-                    System.out.println("Enter word to be searched in cache memory");
+			            System.out.println("Enter word to be searched in cache memory");
 
-                    String cache_word = wordScanner.nextLine();
+			            String cache_word = wordScanner.nextLine();
 
-                    cachedRankingList = search_function(cache_word, cachedRankingList, "./cached_files/");
+			            cachedRankingList = search_function(cache_word, cachedRankingList, "./cached_files/");
 
-                    int rankingListSize = cachedRankingList.size();
+			            int rankingListSize = cachedRankingList.size();
 
-                    if (rankingListSize <= 1) {
-                        System.out.println("word not found in cache so searching in main html page folder");
-                        search_function(cache_word, ranking_list_3, "./htmlToTextPages/");
+			            if (rankingListSize <= 1) {
+			                System.out.println("word not found in cache so searching in main html page folder");
+			                search_function(cache_word, ranking_list_3, "./htmlToTextPages/");
 
-                    }
+			            }
 
-                    break;
-                case 3:
-                    System.out.println("Enter a word to find alternate suggesions for it");
-                    Scanner sc_3 = new Scanner(System.in);
-                    String sugesstion_word = sc_3.nextLine();
-                    WordSuggestions.suggestWord(sugesstion_word);
-                    break;
+			            break;
+			        case 3:
+			            System.out.println("Enter a word to find alternate suggesions for it");
+			            Scanner sc_3 = new Scanner(System.in);
+			            String sugesstion_word = sc_3.nextLine();
+			            WordSuggestions.suggestWord(sugesstion_word);
+			            break;
 
-                default:
-                    exitStatus = 1;
-                    break;
+			        default:
+			            exitStatus = 1;
+			            break;
 
 
-            }
-            System.out.println("====================Enter your option====================");
+			    }
+			    System.out.println("====================Enter your option====================");
 
-            System.out.println("1 for searching word");
-            System.out.println("2 for searching word in cache");
-            System.out.println("3 for providing sugesstions to word");
+			    System.out.println("1 for searching word");
+			    System.out.println("2 for searching word in cache");
+			    System.out.println("3 for providing sugesstions to word");
 
-            choice = sc.nextInt();
+			    choice = sc.nextInt();
+			    
+			} while (exitStatus == 0);
+		}
 
-        } while (exitStatus == 0);
 
     }
 
